4131b1269893d29ca38eb4a9fa7e00e5dd6403b2
xx0tt <307457823@qq.com>
2022-07-06 21:22:46 +0800

    完成了动态表格案例

---
+++ 3_表格案例/components/MyTable.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <table border="1">
+      <thead>
+        <tr>
+          <th>序号</th>
+          <th>姓名</th>
+          <th>年龄</th>
+          <th>头像</th>
+        </tr>
+      </thead>
+      <thead>
+        <tr v-for="item in list" :key="item.id">
+          <!-- <td>{{ item.id }}</td>
+          <td>{{ item.name }}</td>
+          <td>{{ item.age }}</td>
+          <td>{{ item.headImgUrl }}</td> -->
+          <td><slot name="Myid" :row="item"></slot></td>
+          <td><slot name="Myname" :row="item">默认名字</slot></td>
+          <td><slot name="Myage" :row="item"></slot></td>
+          <td><slot name="MyheadImgUrl" :row="item"></slot></td>
+        </tr>
+      </thead>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    list: {
+      type: Array,
+      default: [],
+    },
+  },
+}
+</script>
