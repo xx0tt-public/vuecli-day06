85ebc14d4562e30517b6adf0e8bbe2fa375d6b16
renbuairen <1664844124@qq.com>
2022-07-07 00:26:04 +0800

    短信案例

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,66 @@
-<template></template>
+<template>
+  <div id="box">
+    <div class="hander">
+      <span class="phone" :class="{ count: isClick == 'left' }" @click="leftFn"
+        >手机注册</span
+      >
+      <span
+        class="email"
+        :class="{ count: isClick == 'right' }"
+        @click="rightFn"
+        >邮箱注册</span
+      >
+    </div>
+    <keep-alive>
+      <component :is="componentId"></component>
+    </keep-alive>
+  </div>
+</template>
 
 <script>
-export default {}
+import Phone from './components/Phone.vue'
+import Email from './components/Email.vue'
+export default {
+  data() {
+    return {
+      isClick: 'left',
+      componentId: 'Phone',
+    }
+  },
+  components: {
+    Email,
+    Phone,
+  },
+  methods: {
+    leftFn() {
+      this.componentId = 'Phone'
+      this.isClick = 'left'
+    },
+    rightFn() {
+      this.componentId = 'Email'
+      this.isClick = 'right'
+    },
+  },
+}
 </script>
 
-<style></style>
+<style scoped>
+#box {
+  padding: 40px;
+}
+.hander {
+  /* background-color: orange; */
+  display: flex;
+  justify-content: center;
+  margin: 0 auto;
+  font-size: 24px;
+  margin-top: 10px;
+}
+.phone {
+  margin-right: 40px;
+}
+.count {
+  color: blue;
+  border-bottom: 2px solid blue;
+}
+</style>
---
+++ src/components/Phone.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="main">
+    <p>选择所在地区(实名认证主体所在地)</p>
+    <input type="radio" name="area" value="1" id="DL" />
+    <label for="DL">中国大陆</label>
+    <input type="radio" name="area" value="0" id="HK" />
+    <label for="HK">中国香港</label>
+
+    <p>手机号码</p>
+    <span>+86 </span>
+    <input type="text" placeholder="输入手机号" /><br />
+    <input type="text" placeholder="输入验证码" />
+    <button class="btn">发送验证码</button>
+    <p>登陆密码</p>
+    <input type="password" placeholder="输入登陆密码" /><br />
+    <input type="password" placeholder="再次输入登陆密码" />
+    <p>邀请码（选填）</p>
+    <input type="text" placeholder="输入客户经理邀请码" />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+main {
+  padding: 10px;
+  input[type='radio'] {
+    margin-left: 10px;
+  }
+  input {
+    margin: 10px 0;
+    vertical-align: middle;
+  }
+  button {
+    margin-left: 10px;
+  }
+  p {
+    transform: translateY(20px);
+  }
+}
+.btn {
+  margin-left: 10px;
+  margin-top: 10px;
+  widows: 100px;
+  height: 25px;
+  background-color: rgb(51, 51, 240);
+  line-height: 25px;
+  color: aliceblue;
+}
+</style>
